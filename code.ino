10d2abe809a501632742192e69415be2196f07f2
ByteWire3D <165397383+ByteWire3D@users.noreply.github.com>
2025-02-24 22:33:43 +0100

    Update code.ino
    restructure

--- code.ino
+++ code.ino
@@ -1,31 +1,71 @@
 #include "Adafruit_VL53L1X.h"
+#include <Adafruit_GFX.h>     // Core graphics library
+#include <Adafruit_ST7789.h>  // Hardware-specific library for ST7789
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 
 #define IRQ_PIN 4
 #define XSHUT_PIN 5
 
+// ST7789 TFT module connections
+#define TFT_CS 20   // define chip select pin
+#define TFT_DC 5    // define data/command pin
+#define TFT_RST 21  // define reset pin, or set to -1 and connect to Arduino RESET pin
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+
+// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
+//                                   id, address
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+
+// Initialize Adafruit ST7789 TFT library
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 Adafruit_VL53L1X vl53 = Adafruit_VL53L1X(XSHUT_PIN, IRQ_PIN);
+
+
+int delay_time_l = 100;
+int delay_time_d = 50;
+
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial) delay(10);
 
-  Serial.println(F("Adafruit VL53L1X sensor demo"));
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+
+  tft.init(240, 280);  // Init ST7789 display 240x240 pixel
+  tft.setRotation(1);
+  tft.fillScreen(ST77XX_BLACK);
+  dart_ani();
+
+  /* Initialise the sensor */
+  if (!bno.begin()) {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1)
+      ;
+  }
+
+  delay(1000);
+
+  /* Use external crystal for better accuracy */
+  bno.setExtCrystalUse(true);
 
   Wire.begin();
-  if (! vl53.begin(0x29, &Wire)) {
+  if (!vl53.begin(0x29, &Wire)) {
     Serial.print(F("Error on init of VL sensor: "));
     Serial.println(vl53.vl_status);
-    while (1)       delay(10);
+    while (1) delay(10);
   }
   Serial.println(F("VL53L1X sensor OK!"));
 
-  Serial.print(F("Sensor ID: 0x"));
-  Serial.println(vl53.sensorID(), HEX);
-
-  if (! vl53.startRanging()) {
+  if (!vl53.startRanging()) {
     Serial.print(F("Couldn't start ranging: "));
     Serial.println(vl53.vl_status);
-    while (1)       delay(10);
+    while (1) delay(10);
   }
   Serial.println(F("Ranging started"));
 
@@ -33,7 +73,6 @@ void setup() {
   vl53.setTimingBudget(50);
   Serial.print(F("Timing budget (ms): "));
   Serial.println(vl53.getTimingBudget());
-
   /*
   vl.VL53L1X_SetDistanceThreshold(100, 300, 3, 1);
   vl.VL53L1X_SetInterruptPolarity(0);
@@ -59,42 +98,29 @@ void loop() {
     // data is read out, time for another reading!
     vl53.clearInterrupt();
   }
+
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+  /* The processing sketch expects data as roll, pitch, heading */
+  Serial.print(F("Orientation: "));
+  Serial.print((float)event.orientation.x);
+  Serial.print(F(" "));
+  Serial.print((float)event.orientation.y);
+  Serial.print(F(" "));
+  Serial.print((float)event.orientation.z);
+  Serial.println(F(""));
+
+
+  Serial.print(analogRead(2));
+  Serial.print("   |   ");
+  Serial.print(analogRead(3));
+  Serial.print("   |   voltmes: ");
+  Serial.println(analogRead(4));
+  delay(BNO055_SAMPLERATE_DELAY_MS);
 }
 
-#include <Adafruit_GFX.h>     // Core graphics library
-#include <Adafruit_ST7789.h>  // Hardware-specific library for ST7789
-#include <SPI.h>              // Arduino SPI library
-
-
-// ST7789 TFT module connections
-#define TFT_CS 20  // define chip select pin
-#define TFT_DC 5  // define data/command pin
-#define TFT_RST 21  // define reset pin, or set to -1 and connect to Arduino RESET pin
-
-
-// Initialize Adafruit ST7789 TFT library
-Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
-
-int delay_time_l = 100;
-int delay_time_d = 50;
-
-
-void setup(void) {
-  Serial.begin(9600);
-  Serial.print(F("Hello! ST77xx TFT Test"));
-
-
-  // if the display has CS pin try with SPI_MODE0
-  tft.init(240, 280);  // Init ST7789 display 240x240 pixel
-
-
-  // if the screen is flipped, remove this command
-  tft.setRotation(1);
-
-
-  Serial.println(F("Initialized"));
-
-  tft.fillScreen(ST77XX_BLACK);
+void dart_ani() {
   tft.setCursor(60, 100);
   tft.setTextSize(4);
   //tft.print("D.A.R.T.");
@@ -122,160 +148,4 @@ void setup(void) {
   tft.setCursor(60, 100);
   tft.print("D.A.R.T");
   delay(delay_time_d);
-}
-
-
-void loop() {
-
-
-}
-
-
-
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>
-
-/* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
-   which provides a common 'type' for sensor data and some helper functions.
-
-   To use this driver you will also need to download the Adafruit_Sensor
-   library and include it in your libraries folder.
-
-   You should also assign a unique ID to this sensor for use with
-   the Adafruit Sensor API so that you can identify this particular
-   sensor in any data logs, etc.  To assign a unique ID, simply
-   provide an appropriate value in the constructor below (12345
-   is used by default in this example).
-
-   Connections
-   ===========
-   Connect SCL to analog 5
-   Connect SDA to analog 4
-   Connect VDD to 3.3-5V DC
-   Connect GROUND to common ground
-
-   History
-   =======
-   2015/MAR/03  - First release (KTOWN)
-*/
-
-/* Set the delay between fresh samples */
-#define BNO055_SAMPLERATE_DELAY_MS (100)
-
-// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
-//                                   id, address
-Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
-
-/**************************************************************************/
-/*
-    Displays some basic information on this sensor from the unified
-    sensor API sensor_t type (see Adafruit_Sensor for more information)
-*/
-/**************************************************************************/
-void displaySensorDetails(void)
-{
-  sensor_t sensor;
-  bno.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
-}
-
-/**************************************************************************/
-/*
-    Arduino setup function (automatically called at startup)
-*/
-/**************************************************************************/
-void setup(void)
-{
-  Serial.begin(115200);
-  Serial.println("Orientation Sensor Test"); Serial.println("");
-
-  /* Initialise the sensor */
-  if(!bno.begin())
-  {
-    /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while(1);
-  }
-   
-  delay(1000);
-
-  /* Use external crystal for better accuracy */
-  bno.setExtCrystalUse(true);
-   
-  /* Display some basic information on this sensor */
-  displaySensorDetails();
-}
-
-/**************************************************************************/
-/*
-    Arduino loop function, called once 'setup' is complete (your own code
-    should go here)
-*/
-/**************************************************************************/
-void loop(void)
-{
-  /* Get a new sensor event */
-  sensors_event_t event;
-  bno.getEvent(&event);
-
-  /* Board layout:
-         +----------+
-         |         *| RST   PITCH  ROLL  HEADING
-     ADR |*        *| SCL
-     INT |*        *| SDA     ^            /->
-     PS1 |*        *| GND     |            |
-     PS0 |*        *| 3VO     Y    Z-->    \-X
-         |         *| VIN
-         +----------+
-  */
-
-  /* The processing sketch expects data as roll, pitch, heading */
-  Serial.print(F("Orientation: "));
-  Serial.print((float)event.orientation.x);
-  Serial.print(F(" "));
-  Serial.print((float)event.orientation.y);
-  Serial.print(F(" "));
-  Serial.print((float)event.orientation.z);
-  Serial.println(F(""));
-
-  /* Also send calibration data for each sensor. */
-  uint8_t sys, gyro, accel, mag = 0;
-  bno.getCalibration(&sys, &gyro, &accel, &mag);
-  Serial.print(F("Calibration: "));
-  Serial.print(sys, DEC);
-  Serial.print(F(" "));
-  Serial.print(gyro, DEC);
-  Serial.print(F(" "));
-  Serial.print(accel, DEC);
-  Serial.print(F(" "));
-  Serial.println(mag, DEC);
-
-  delay(BNO055_SAMPLERATE_DELAY_MS);
-}
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
-  pinMode(4, INPUT);
-}
-
-void loop() {
-  Serial.print(analogRead(2));
-  Serial.print("   |   ");
-  Serial.print(analogRead(3));
-  Serial.print("   |   voltmes: ");
-  Serial.println(analogRead(4));
-  delay(100);
 }
