0fd32ba29093fbba2a82f0be6c27eea65db53882
ByteWire3D <165397383+ByteWire3D@users.noreply.github.com>
2025-02-27 22:58:27 +0100

    Update code.ino

--- code.ino
+++ code.ino
@@ -23,6 +23,21 @@
 
 #define right_buttons 2
 #define left_buttons 3
+
+#define NUM_SAMPLES 100  // Number of readings for averaging
+
+#define DEBOUNCE_DELAY 1000  // 50ms debounce time
+
+// Store the last stable states
+int lastStableRight = -1;
+int lastStableLeft = -1;
+unsigned long lastDebounceTimeRight = 0;
+unsigned long lastDebounceTimeLeft = 0;
+
+float batt_volt_samples[NUM_SAMPLES] = { 0 };  // Array to store voltage readings
+int sample_index = 0;                          // Index for tracking position
+float filtered_batt_volt = 0;                  // Filtered voltage valu
+
 bool right_up_wasdown = false;
 bool right_down_wasdown = false;
 bool left_up_wasdown = false;
@@ -41,6 +56,15 @@ int delay_time_d = 50;
 
 float batt_volt;
 float batt_percent;
+int batt_per;
+int batt_perc = 0;
+
+float roll;
+float pitch;
+float yaw;
+
+int temprature = 0;
+bool update = true;
 void setup() {
   Serial.begin(115200);
 
@@ -53,6 +77,11 @@ void setup() {
   tft.fillScreen(ST77XX_BLACK);
   dart_ani();
 
+
+  float initial_value = (4.11 / 2860) * analogRead(4);
+  for (int i = 0; i < NUM_SAMPLES; i++) {
+    batt_volt_samples[i] = initial_value;
+  }
   /* Initialise the sensor */
   if (!bno.begin()) {
     /* There was a problem detecting the BNO055 ... check your connections */
@@ -61,7 +90,6 @@ void setup() {
       ;
   }
 
-  delay(1000);
 
   /* Use external crystal for better accuracy */
   bno.setExtCrystalUse(true);
@@ -126,28 +154,252 @@ void loop() {
   Serial.println(F(""));
 
 */
-
-  // Serial.print(analogRead(right_buttons));
-  // Serial.print("   |   ");
-  // Serial.print(analogRead(left_buttons));
-  //Serial.print("   |   voltmes: ");
-  //Serial.println(analogRead(4));
-
+/*
+   Serial.print(analogRead(right_buttons));
+   Serial.print("   |   ");
+   Serial.print(analogRead(left_buttons));
+  Serial.print("   |   voltmes: ");
+  Serial.println(analogRead(4));
+  */
+  live_mes();
+  if (update) {
+    //show_display();
+    update = false;
+  }
   handle_buttons();
-
-  //messure_volt();
-  delay(BNO055_SAMPLERATE_DELAY_MS);
+  //live_angles();
+  //get_temp();
+  messure_volt();
+  delay(100);
 }
 void messure_volt() {
-  batt_volt = map(analogRead(4), 0, 3000, 0, 4.2);
-  batt_percent = map(analogRead(4), 0, 3000, 0, 100);
+  batt_volt = (4.11 / 2860) * analogRead(4);
+
+  batt_volt_samples[sample_index] = batt_volt;
+  sample_index = (sample_index + 1) % NUM_SAMPLES;  // Loop back after filling the array
+
+  // Compute the moving average
+  float sum = 0;
+  for (int i = 0; i < NUM_SAMPLES; i++) {
+    sum += batt_volt_samples[i];
+  }
+  filtered_batt_volt = sum / NUM_SAMPLES;
+
+  // Convert to percentage
+  float batt_percent = ((filtered_batt_volt - 3.3) / 0.9) * 100;
+  int batt_per = int(trunc(batt_percent));
+
+  batt_per = int(trunc(batt_percent));
+  if (batt_per != batt_perc) {
+    batt_perc = batt_per;
+    update = true;
+  }
+  /*
   Serial.print(batt_volt);
   Serial.print("    |   ");
   Serial.print(batt_percent);
-  Serial.print("   |   voltmes: ");
-  Serial.println(analogRead(4));
+  Serial.print("    |   ");
+  Serial.println(batt_perc);
+  */
+  //Serial.print("   |   voltmes: ");
+  //Serial.println(analogRead(4));
+}
+void show_display() {
+  drawHeader("Mode menu", ST77XX_WHITE, ST77XX_RED);
+  int x = 20;
+  int y = 36;
+  tft.setCursor(x, y);
+  tft.setTextSize(2);
+  tft.print("Distance");
+  y += 24;
+  tft.setCursor(x, y);
+  tft.setTextSize(2);
+  tft.print("Angle");
+  y += 24;
+  tft.setCursor(x, y);
+  tft.setTextSize(2);
+  tft.print("Live");
 }
 
+void ver_hor_tot(int mode, distace){
+  
+}
+void live_mes() {
+  drawHeader("Live Measurements", ST77XX_WHITE, ST77XX_RED);
+  int16_t distance;
+
+  if (vl53.dataReady()) {
+    // new measurement for the taking!
+    distance = vl53.distance();
+    if (distance == -1) {
+      // something went wrong!
+      Serial.print(F("Couldn't get distance: "));
+      Serial.println(vl53.vl_status);
+      return;
+    }
+   // Serial.print(F("Distance: "));
+   // Serial.print(distance);
+   // Serial.println(" mm");
+
+    // data is read out, time for another reading!
+    vl53.clearInterrupt();
+  }
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+  pitch = event.orientation.z;
+  roll = event.orientation.y;
+  yaw = event.orientation.x;
+
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+  tft.setTextSize(2);
+  int x = 20;
+  int y = 36;
+
+  tft.setCursor(x, y);
+  tft.print("Distance: ");
+
+  x = 20 + calculateLength("Distance: ");
+  tft.setCursor(x, y);
+  tft.print(distance);
+  tft.print("       ");
+  y += 24;
+  x = 20;
+  tft.setCursor(x, y);
+  tft.setTextSize(2);
+  tft.print("Pitch: ");
+  x = 20 + calculateLength("Pitch: ");
+  tft.setCursor(x, y);
+  tft.print(pitch);
+  tft.print("    ");
+  y += 24;
+  x = 20;
+  tft.setCursor(x, y);
+  tft.setTextSize(2);
+  tft.print("Roll: ");
+  x = 20 + calculateLength("Roll: ");
+  tft.setCursor(x, y);
+  tft.print(roll);
+  tft.print("     ");
+  y += 24;
+  x = 20;
+  tft.setCursor(x, y);
+  tft.setTextSize(2);
+  tft.print("Yaw: ");
+  x = 20 + calculateLength("Yaw: ");
+  tft.setCursor(x, y);
+  tft.print(yaw);
+  tft.print("     ");
+}
+
+void drawHeader(const char* headerText, uint16_t color, uint16_t barColor) {
+  int headerPos = (280 - calculateLength(headerText)) / 2;  // centers header
+  tft.setCursor(headerPos, 2);
+  tft.setTextColor(color, ST77XX_BLACK);
+  tft.setTextSize(2);
+  tft.print(headerText);
+  tft.fillRoundRect(16, 20, 248, 5, 5, barColor);
+  float inv_batt_perc = 100 - batt_perc;
+  int x = 246 - (229 * inv_batt_perc / 100);
+  int l = 263 - x;
+  tft.fillRoundRect(x, 21, l, 3, 3, ST77XX_BLACK);
+  //tft.fillRoundRect(x, 21, l, 3, 3, ST77XX_BLACK);
+}
+int calculateLength(const char* word) {
+  int length = 0;
+  for (int i = 0; word[i] != '\0'; i++) {
+    if (word[i] == ' ') {
+      length += 10;
+    } else {
+      length += 12;
+    }
+  }
+  return length;
+}
+
+int calculateLengthSize(char* word, int size) {
+  if (size == 1) {
+    int length = 0;
+    for (int i = 0; word[i] != '\0'; i++) {
+      if (word[i] == ' ') {
+        length += 6;
+      } else {
+        length += 8;
+      }
+    }
+    return length;
+  }
+  if (size == 2) {
+    int length = 0;
+    for (int i = 0; word[i] != '\0'; i++) {
+      if (word[i] == ' ') {
+        length += 10;
+      } else {
+        length += 12;
+      }
+    }
+    return length;
+  }
+  if (size == 3) {
+    int length = 0;
+    for (int i = 0; word[i] != '\0'; i++) {
+      if (word[i] == ' ') {
+        length += 16;
+      } else {
+        length += 18;
+      }
+    }
+    return length;
+  }
+  if (size == 4) {
+    int length = 0;
+    for (int i = 0; word[i] != '\0'; i++) {
+      if (word[i] == ' ') {
+        length += 20;
+      } else {
+        length += 24;
+      }
+    }
+    return length;
+  }
+}
+void live_angles() {
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+  pitch = event.orientation.z;
+  roll = event.orientation.y;
+  yaw = event.orientation.x;
+
+  Serial.print(F("pitch: "));
+  Serial.print(pitch);
+  Serial.print(F(" "));
+  Serial.print(F("roll: "));
+  Serial.print(roll);
+  Serial.print(F(" "));
+  Serial.print(F("yaw: "));
+  Serial.print(yaw);
+  Serial.println(F(""));
+
+  /*
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+
+  Serial.print("X: ");
+  Serial.print(euler.x());
+  Serial.print(" Y: ");
+  Serial.print(euler.y());
+  Serial.print(" Z: ");
+  Serial.print(euler.z());
+  Serial.print("\n");
+  */
+}
+
+void get_temp() {
+  if (bno.getTemp() != temprature) {
+    temprature = bno.getTemp();
+    update = true;
+  }
+}
 void handle_buttons() {
   if (Button_IsDown(right_up)) {
     right_up_wasdown = true;
@@ -189,10 +441,11 @@ bool Button_IsDown(int button) {
   }
   if (button == right_down) {
     //right down
-    if (analogRead(right_buttons) >= 2000 && analogRead(right_buttons) <= 2300) {
+    if (analogRead(right_buttons) >= 40 && analogRead(right_buttons) <= 100) {
       return true;
     }
   }
+
   if (button == left_up) {
     //left up
     if (analogRead(left_buttons) == 4095) {
@@ -201,7 +454,7 @@ bool Button_IsDown(int button) {
   }
   if (button == left_down) {
     //left down
-    if (analogRead(left_buttons) <= 250) {
+    if (analogRead(left_buttons) <= 50) {
       return true;
     }
   }
@@ -211,30 +464,130 @@ bool Button_IsDown(int button) {
 bool Button_IsUP(int button) {
   if (button == right_up) {
     //right up
-    if (analogRead(right_buttons) <= 2000) {
+    if (analogRead(right_buttons) <= 40) {
       return true;
     }
   }
   if (button == right_down) {
     //right down
-    if (analogRead(right_buttons) <= 2000) {
+    if (analogRead(right_buttons) <= 40) {
       return true;
     }
   }
   if (button == left_up) {
     //left up
-    if (analogRead(left_buttons) > 250 && analogRead(left_buttons) < 4095) {
+    if (analogRead(left_buttons) > 50 && analogRead(left_buttons) < 4095) {
       return true;
     }
   }
   if (button == left_down) {
     //left down
-    if (analogRead(left_buttons) > 250 && analogRead(left_buttons) < 4095) {
+    if (analogRead(left_buttons) > 50 && analogRead(left_buttons) < 4095) {
       return true;
     }
   }
   return false;
 }
+
+/*
+bool Button_IsDown(int button) {
+  int rightValue = analogRead(right_buttons);
+  int leftValue = analogRead(left_buttons);
+  
+  unsigned long currentTime = millis();
+
+  // Right Up Button
+  if (button == right_up) {
+    if (rightValue >= 4000) {
+      if ((currentTime - lastDebounceTimeRight) > DEBOUNCE_DELAY) {
+        lastStableRight = right_up;
+        return true;
+      }
+    }
+  }
+
+  // Right Down Button
+  if (button == right_down) {
+    if (rightValue >= 2100 && rightValue <= 2250) {
+      if ((currentTime - lastDebounceTimeRight) > DEBOUNCE_DELAY) {
+        lastStableRight = right_down;
+        return true;
+      }
+    }
+  }
+
+  // Left Up Button
+  if (button == left_up) {
+    if (leftValue >= 4000) {
+      if ((currentTime - lastDebounceTimeLeft) > DEBOUNCE_DELAY) {
+        lastStableLeft = left_up;
+        return true;
+      }
+    }
+  }
+
+  // Left Down Button
+  if (button == left_down) {
+    if (leftValue <= 250) {
+      if ((currentTime - lastDebounceTimeLeft) > DEBOUNCE_DELAY) {
+        lastStableLeft = left_down;
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+bool Button_IsUP(int button) {
+  int rightValue = analogRead(right_buttons);
+  int leftValue = analogRead(left_buttons);
+
+  unsigned long currentTime = millis();
+
+  // Right Up Released
+  if (button == right_up) {
+    if (rightValue <= 4000) {
+      if ((currentTime - lastDebounceTimeRight) > DEBOUNCE_DELAY) {
+        lastStableRight = -1;
+        return true;
+      }
+    }
+  }
+
+  // Right Down Released
+  if (button == right_down) {
+    if (rightValue <= 2000) {
+      if ((currentTime - lastDebounceTimeRight) > DEBOUNCE_DELAY) {
+        lastStableRight = -1;
+        return true;
+      }
+    }
+  }
+
+  // Left Up Released
+  if (button == left_up) {
+    if (leftValue > 250 && leftValue < 4095) {
+      if ((currentTime - lastDebounceTimeLeft) > DEBOUNCE_DELAY) {
+        lastStableLeft = -1;
+        return true;
+      }
+    }
+  }
+
+  // Left Down Released
+  if (button == left_down) {
+    if (leftValue > 250 && leftValue < 4095) {
+      if ((currentTime - lastDebounceTimeLeft) > DEBOUNCE_DELAY) {
+        lastStableLeft = -1;
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+*/
 void dart_ani() {
   tft.setCursor(60, 100);
   tft.setTextSize(4);
@@ -263,4 +616,7 @@ void dart_ani() {
   tft.setCursor(60, 100);
   tft.print("D.A.R.T");
   delay(delay_time_d);
+
+  delay(1000);
+  tft.fillScreen(ST77XX_BLACK);
 }
